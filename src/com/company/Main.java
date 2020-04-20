26a3b138fbee029a1d0febda75f27c930fd61426
Ming Lin <Ming.Lin@morningstar.com>
2020-04-20 19:51:54 +0800

    test 003

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
 	// write your code here
         System.out.println("test 001...");
         System.out.println("test 002...");
+        System.out.println("test 003...");
     }
 }
